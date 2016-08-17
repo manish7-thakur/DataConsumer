025a9f267535496b2afcd2f9838a343bab64dc1d
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-08-17 18:23:42 +0530

    Created test data & refactored code

---
+++ src/test/scala/TestData.scala
@@ -0,0 +1,13 @@
+
+import scala.collection.immutable.Queue
+
+trait TestData {
+
+  val r1Queue = Queue(DataElement("2", "R1"))
+  val r2Queue = Queue()
+  val g1Queue = Queue(DataElement("3", "G1"))
+  val g2Queue = Queue(DataElement("4", "G2"), DataElement("5", "G2"))
+  val b1Queue = Queue(DataElement("1", "B1"))
+  val b2Queue = Queue(DataElement("3", "B2"))
+
+}
